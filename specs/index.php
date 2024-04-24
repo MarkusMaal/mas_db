91daefec605b04d7dd9d57bd8c87ea44640f53ae
Markus Maal <markus.maal@gmail.com>
2024-04-24 15:59:35 +0300

    Update specs

--- specs/index.php
+++ specs/index.php
@@ -9,12 +9,10 @@
     <li><?php echo ms("Video card", "Videokaart");?>: ASRock AMD RX7600 8GB OC</li>
     <li><?php echo ms("Power supply", "Toiteplokk");?>: Chieftec ELP-700S (700W, 200-240V)</li>
     <li><?php echo ms("Processor cooling", "Protsessori jahutus");?>: Pure Rock Slim 2</li>
-    <li><?php echo ms("Misc. cooling", "Muu jahutus");?>: 1 <?php echo ms("unknown PWM fan", "tundmatu PWM ventilaator");?></li>
-    <li><?php echo ms("Internal card reader", "Sisemine kaardilugeja");?> (<?php echo ms("chip", "kiipkaart");?>+SD/MMC+CF/MD+MS+xD)</li>
-    <li><?php echo ms("Disc writer", "Plaadi kirjutaja");?>: Lite-ON DVD&#177;RW 48x</li>
+    <li><?php echo ms("Misc. cooling", "Muu jahutus");?>: 4x <?php echo ms("case PWM fans", "korpuse PWM ventilaatorid");?></li>
     <li><?php echo ms("Monitors", "Monitorid");?>: 1x1920x1080@144Hz VA + 2x1280x1024@75Hz TN</li>
     <li><?php echo ms("Front panel I/O", "Esipaneeli pordid");?>: 2xUSB3+2x3.5mm HD audio</li>
-    <li><?php echo ms("Case", "Korpus");?>: Ordi Samba 9th Gen ATX <?php echo ms("Mid-tower", "Miditorn");?></li>
+    <li><?php echo ms("Case", "Korpus");?>: Chieftec Hunter GS-01B-OP ATX <?php echo ms("Mid-tower", "Miditorn");?></li>
     <li><?php echo ms("Modified UEFI boot logo", "Modifitseeritud UEFI buutimislogo");?>: <?= ms("Yes", "Jah"); ?> (<a href="/mas_db/images/BIOS_logo3.png"><?= ms("preview", "eelvaade") ?></a>)</li>
 </ul>
 <h3><?php echo ms("Laptop", "Sülearvuti");?></h3>
