a7a347b4e30c835abebd9904741b6cb86f341976
Markus Maal <markus.maal@gmail.com>
2024-09-23 12:27:06 +0300

    Modified monitor specs

--- specs/index.php
+++ specs/index.php
@@ -10,7 +10,7 @@
     <li><?php echo ms("Power supply", "Toiteplokk");?>: Chieftec ELP-700S (700W, 200-240V)</li>
     <li><?php echo ms("Processor cooling", "Protsessori jahutus");?>: Pure Rock Slim 2</li>
     <li><?php echo ms("Misc. cooling", "Muu jahutus");?>: 4x <?php echo ms("case PWM fans", "korpuse PWM ventilaatorid");?></li>
-    <li><?php echo ms("Monitors", "Monitorid");?>: 1x1920x1080@144Hz VA + 2x1280x1024@75Hz TN</li>
+    <li><?php echo ms("Monitors", "Monitorid");?>: 1x5120x1440@165Hz VA</li>
     <li><?php echo ms("Front panel I/O", "Esipaneeli pordid");?>: 2xUSB3+2x3.5mm HD audio</li>
     <li><?php echo ms("Case", "Korpus");?>: Chieftec Hunter GS-01B-OP ATX <?php echo ms("Mid-tower", "Miditorn");?></li>
     <li><?php echo ms("Modified UEFI boot logo", "Modifitseeritud UEFI buutimislogo");?>: <?= ms("Yes", "Jah"); ?> (<a href="/mas_db/images/BIOS_logo3.png"><?= ms("preview", "eelvaade") ?></a>)</li>
