a58aa66158703a6dfe059b296162b224089df9ba
Markus Maal <markus.maal@gmail.com>
2024-11-25 18:16:06 +0200

    Update specifications

--- specs/index.php
+++ specs/index.php
@@ -3,7 +3,7 @@
 <ul>
     <li><?php echo ms("Processor", "Protsessor");?>: AMD Ryzen 7 5700X (<?php echo ms("8c/16t", "8t/16l");?>)</li>
     <li><?php echo ms("Memory", "Mälu");?>: 32GB (<?php echo ms("slots", "pesad");?> A2 &amp; B2, 3200MHz, CL16)</li>
-    <li><?php echo ms("Storage", "Andmekandjad");?>: WD Blue 2TB M.2 NVMe SSD + 2TB Toshiba 7200RPM HDD</li>
+    <li><?php echo ms("Storage", "Andmekandjad");?>: WD Blue 2TB M.2 NVMe SSD + 4TB Samsung SSD 990 PRO with Heatsink</li>
     <li><?php echo ms("Motherboard", "Emaplaat");?>: Asus TUF GAMING B550-PLUS</li>
     <li><?php echo ms("Motherboard I/O", "Emaplaadi pordid");?>: 1xUSB3 10Gbps Type-A, 1xUSB3 Type-C, 4xUSB3 Type-A, 2xUSB2 Type-A, 2.5Gbps Ethernet, HDMI, DP, 3.5mm audio, SPDIF <?php echo ms("output", "väljund");?></li>
     <li><?php echo ms("Video card", "Videokaart");?>: Sapphire&reg; NITRO+ AMD Radeon&#8482; RX 7800 XT 16GB</li>
@@ -13,6 +13,7 @@
     <li><?php echo ms("Monitors", "Monitorid");?>: 1x5120x1440@165Hz VA</li>
     <li><?php echo ms("Front panel I/O", "Esipaneeli pordid");?>: 2xUSB3+2x3.5mm HD audio</li>
     <li><?php echo ms("Case", "Korpus");?>: Chieftec Hunter GS-01B-OP ATX <?php echo ms("Mid-tower", "Miditorn");?></li>
+    <li><?php echo ms("UPS", "Katkematu toiteallikas (UPS)");?>: Eaton 5E G2 2200</li>
     <li><?php echo ms("Modified UEFI boot logo", "Modifitseeritud UEFI buutimislogo");?>: <?= ms("Yes", "Jah"); ?> (<a href="/mas_db/images/BIOS_logo3.png"><?= ms("preview", "eelvaade") ?></a>)</li>
 </ul>
 <h3><?php echo ms("Laptop", "Sülearvuti");?></h3>
